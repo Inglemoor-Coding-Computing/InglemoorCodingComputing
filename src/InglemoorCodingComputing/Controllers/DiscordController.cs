5dad132704003946f212b45ad1e22be45fee75d4
albert-du <52804499+albert-du@users.noreply.github.com>
2022-09-16 21:01:56 -0700

    discord redirection

---
+++ src/InglemoorCodingComputing/Controllers/DiscordController.cs
@@ -0,0 +1,10 @@
+﻿namespace InglemoorCodingComputing.Controllers;
+
+[Route("/[controller]")]
+[ApiController]
+public class DiscordController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() =>
+        Redirect("https://discord.gg/4s6stUApbc");
+}
