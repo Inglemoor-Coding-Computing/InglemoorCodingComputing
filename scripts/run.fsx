8b521fbc07987d52e97c996cb460432847162c28
albert-du <52804499+albert-du@users.noreply.github.com>
2022-09-25 15:01:25 -0700

    Update run.fsx

--- scripts/run.fsx
+++ scripts/run.fsx
@@ -8,40 +8,52 @@ open System.Threading
 
 while true do
     printfn "starting ..."
+
+    cd ["bin"]
+
+    let bin = Environment.CurrentDirectory
+
+    let proc =
+        try
+            let file = 
+                DirectoryInfo(".").EnumerateFiles()
+                |> Seq.find (fun x -> x.Name = "InglemoorCodingComputing.dll")
+
+            printfn $"EXECUTING: {file.FullName}"
+
+            execP "dotnet" $" \"{file.FullName}\""
+            |> Some
+        with _ -> 
+            None
+
     try
-        cd ["bin"]
-
-        let bin = Environment.CurrentDirectory
-
-        let file = 
-            DirectoryInfo(".").EnumerateFiles()
-            |> Seq.find (fun x -> x.Name = "InglemoorCodingComputing.dll")
-
-        let proc = execP "sudo" $"-E dotnet \"{file.FullName}\""
-
         cd []
         
         let mutable broken = false;
-        while not broken do
-            // check every 10 seconds for a new version
+        while not broken && (match proc with None -> false | Some x -> not x.HasExited) do
+            // check every 5 seconds
             Thread.Sleep 10000
+            printfn "Checking for new version or crash"
             DirectoryInfo(".").EnumerateFiles()
             |> Seq.tryFind (fun x -> x.Name = "package.zip")
             |> function
             | Some zip -> 
-                proc.Kill();
+                printfn "Updating"
+                proc 
+                |> Option.iter (fun proc -> proc.Kill())
 
                 Directory.Delete(bin, true)
                 cd ["bin"]
 
                 ZipFile.ExtractToDirectory(zip.FullName, bin)
 
+                File.Delete zip.FullName
+
                 broken <- true;
             | _ -> 
                 ()
-    with _ -> 
+    with _ ->
         ()
-
     printfn "process ended, waiting to restart"
     Thread.Sleep 5000
     printf "re"
