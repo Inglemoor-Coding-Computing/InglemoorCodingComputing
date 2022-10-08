314c44df4abbdbd706933e462fd8921adcf8d458
albert-du <52804499+albert-du@users.noreply.github.com>
2022-10-07 19:55:34 -0700

    caching meetings

--- src/InglemoorCodingComputing/Services/MeetingsService.cs
+++ src/InglemoorCodingComputing/Services/MeetingsService.cs
@@ -1,39 +1,63 @@
 ﻿namespace InglemoorCodingComputing.Services;
 
+using System.Collections.Concurrent;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Cosmos.Linq;
 
-public sealed class MeetingsService : IMeetingsService
+public sealed class MeetingsService : IMeetingsService, IDisposable
 {
+    private readonly ICacheEventService _cacheEventService;
     private readonly Container _container;
+
+    private readonly ConcurrentDictionary<int, ConcurrentDictionary<Guid, Meeting>> _cache = new();
 
     public event EventHandler? Changed;
     private void OnChange() => Changed?.Invoke(this, new());
-    public MeetingsService(IConfiguration configuration, CosmosClient cosmosClient)
+    public MeetingsService(IConfiguration configuration, CosmosClient cosmosClient, ICacheEventService cacheEventService)
     {
+        _cacheEventService = cacheEventService;
+        _cacheEventService.CacheClearing += OnCacheClearing;
         _container = cosmosClient.GetContainer(configuration["Cosmos:DatabaseName"], configuration["Cosmos:MeetingsContainer"]);
     }
+
+    private void OnCacheClearing(Action<string, bool> report)
+    {
+        _cache.Clear();
+        report(nameof(MeetingsService), true);
+    }
+
+    public void Dispose() =>
+        _cacheEventService.CacheClearing -= OnCacheClearing;
 
     public async Task CreateAsync(Meeting meeting)
     {
         await _container.CreateItemAsync(meeting, partitionKey: new(meeting.Id.ToString()));
+        _cache[meeting.Year][meeting.Id] = meeting;
         OnChange();
     }
 
     public async Task DeleteAsync(Guid id)
     {
+        foreach (var year in _cache)
+            year.Value.TryRemove(id, out _);
         await _container.DeleteItemAsync<Meeting>(id.ToString(), new(id.ToString()));
         OnChange();
     }
 
     public async IAsyncEnumerable<Meeting> GetMeetingsAsync(int year)
     {
-        var iterator = _container.GetItemLinqQueryable<Meeting>().Where(x => x.Year == AppUser.AcademicYear).ToFeedIterator();
-        while (iterator.HasMoreResults)
+        if (!_cache.TryGetValue(year, out var _))
         {
-            foreach (var x in await iterator.ReadNextAsync())
-                yield return x;
+            _cache[year] = new();
+            var iterator = _container.GetItemLinqQueryable<Meeting>().Where(x => x.Year == year).ToFeedIterator();
+            while (iterator.HasMoreResults)
+            {
+                foreach (var x in await iterator.ReadNextAsync())
+                    _cache[year][x.Id] = x;
+            }
         }
+        foreach (var meeting in _cache[year])
+            yield return meeting.Value;
     }
 
     public async Task<Meeting?> NextAsync(DateTime local)
@@ -71,6 +95,7 @@ public sealed class MeetingsService : IMeetingsService
     public async Task UpdateAsync(Meeting meeting)
     {
         await _container.ReplaceItemAsync(meeting, meeting.Id.ToString(), new(meeting.Id.ToString()));
+        _cache[meeting.Year][meeting.Id] = meeting;
         OnChange();
     }
 }
