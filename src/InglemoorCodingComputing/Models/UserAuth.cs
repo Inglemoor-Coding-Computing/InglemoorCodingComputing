4535418cb2ca8d48892b8314ee23d1f856031b96
Albert Du <52804499+albert-du@users.noreply.github.com>
2023-02-11 20:29:45 -0800

    fix typo

--- src/InglemoorCodingComputing/Models/UserAuth.cs
+++ src/InglemoorCodingComputing/Models/UserAuth.cs
@@ -3,7 +3,7 @@ namespace InglemoorCodingComputing.Models;
 public record UserAuth(Guid Id, string Email, bool IsAdmin, Argon2idHash? Hash)
 {
     public string? GoogleId { get; init; }
-    public IReadOnlyList<LoginAttempt> LoginAttepts { get; init; } = Array.Empty<LoginAttempt>();
+    public IReadOnlyList<LoginAttempt> LoginAttempts { get; init; } = Array.Empty<LoginAttempt>();
     public DateTime? SecurityTimeStamp { get; init; }
 }
 
